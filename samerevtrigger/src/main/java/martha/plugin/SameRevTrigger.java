5a6cf5cfa28bc71c91ab1e20d6dadee493be286a
martha tiambeng <mtiamben@marting.lrdc.lexmark.ds>
2011-02-23 17:40:27 +0800

    adding files

---
+++ samerevtrigger/src/main/java/martha/plugin/SameRevTrigger.java
@@ -0,0 +1,114 @@
+package martha.plugin;
+import hudson.Launcher;
+import hudson.Extension;
+import hudson.tasks.BuildStepMonitor;
+import hudson.model.AbstractBuild;
+import hudson.model.BuildListener;
+import hudson.model.AbstractProject;
+import hudson.model.Action;
+import hudson.model.Cause;
+import hudson.model.Cause.RemoteCause;
+import hudson.model.Hudson;
+import hudson.model.Result;
+import hudson.scm.SameRevisionAction;
+import hudson.tasks.Builder;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.Publisher;
+import hudson.tasks.Recorder;
+import java.util.Iterator;
+import net.sf.json.JSONObject;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
+
+/**
+ * Sample {@link Builder}.
+ *
+ * <p>
+ * When the user configures the project and enables this builder,
+ * {@link DescriptorImpl#newInstance(StaplerRequest)} is invoked
+ * and a new {@link HelloWorldBuilder} is created. The created
+ * instance is persisted to the project configuration XML by using
+ * XStream, so this allows you to use instance fields (like {@link #name})
+ * to remember the configuration.
+ *
+ * <p>
+ * When a build is performed, the {@link #perform(AbstractBuild, Launcher, BuildListener)} method
+ * will be invoked. 
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public class SameRevTrigger extends Recorder {
+
+    private final String name;
+
+    // Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
+    @DataBoundConstructor
+    public SameRevTrigger(String name) {
+        this.name = name;
+    }
+
+    /**
+     * We'll use this from the <tt>config.jelly</tt>.
+     */
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
+        if (build.getResult().equals(Result.FAILURE)) {
+            Iterator i = build.getCauses().iterator();
+            while(i.hasNext()) {
+                Cause c = (Cause) i.next();
+                listener.getLogger().println(c);
+                if (c instanceof RemoteCause) {
+                    Iterator it = build.getActions().iterator();
+                    while (it.hasNext()) {
+                        Action a = (Action) it.next();
+                        listener.getLogger().println(a);
+                        if (a instanceof SameRevisionAction){
+                            Hudson hudson = Hudson.getInstance();
+                            AbstractProject p = hudson.getItemByFullName("SameRev", AbstractProject.class);
+                            p.scheduleBuild(null);
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public BuildStepMonitor getRequiredMonitorService() {
+        return BuildStepMonitor.NONE;
+    }
+
+    /**
+     * Descriptor for {@link HelloWorldBuilder}. Used as a singleton.
+     * The class is marked as public so that it can be accessed from views.
+     *
+     * <p>
+     * See <tt>views/hudson/plugins/hello_world/HelloWorldBuilder/*.jelly</tt>
+     * for the actual HTML fragment for the configuration screen.
+     */
+    @Extension // this marker indicates Hudson that this is an implementation of an extension point.
+    public static final class DescriptorImpl extends BuildStepDescriptor<Publisher> {
+        
+        public boolean isApplicable(Class<? extends AbstractProject> aClass) {
+            // indicates that this builder can be used with all kinds of project types 
+            return true;
+        }
+        @Override
+        public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
+
+            return super.configure(req,formData);
+        }
+        /**
+         * This human readable name is used in the configuration screen.
+         */
+        public String getDisplayName() {
+            return "Same Revision Trigger";
+        }
+    }
+}
+
